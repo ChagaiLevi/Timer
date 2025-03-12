26e905e34ce1631fd53f4e1f8477b1debed051bb
Chagai Levi <schagailevi@gmail.com>
2025-03-12 23:26:59 +0200

    Stage 2

--- js/script.js
+++ js/script.js
@@ -1,24 +1,53 @@
 "use strict";
-let hours = 0, minutes = 0, seconds = 0;
 const timerClass = document.querySelector('#timer');
-const timer = setInterval(() => {
-    seconds++;
-    if (seconds === 60) {
-        seconds = 0;
-        minutes++;
-    }
-    else if (minutes === 60 && seconds === 60) {
-        minutes = 0;
-        seconds = 0;
-        hours++;
-    }
-    else if (hours === 24 && minutes === 60 && seconds === 60) {
-        timerClass.innerHTML = `
-      ${timerClass.innerHTML}
-      <div class="error"> <span class="warning-icon">!</span>You have reached your time limit!<div>
-    `;
-        clearInterval(timer);
+const startbtn = document.querySelector('#start');
+const stopbtn = document.querySelector('#stop');
+const resetbtn = document.querySelector('#reset');
+let hours = 0, minutes = 0, seconds = 0;
+let timer = null;
+let informer = false;
+startbtn.addEventListener('click', () => startTimer());
+stopbtn.addEventListener('click', () => stopTimer());
+resetbtn.addEventListener('click', () => resetTimer());
+function startTimer() {
+    if (informer) {
         return;
     }
-    timerClass.innerHTML = `${hours <= 9 ? `0${hours}` : hours}:${minutes <= 9 ? `0${minutes}` : minutes}:${seconds <= 9 ? `0${seconds}` : seconds}`;
-}, 1000);
+    timer = setInterval(() => {
+        seconds++;
+        startbtn.innerHTML === 'Resume' ? startbtn.innerHTML = 'Start' : null;
+        if (seconds === 60) {
+            seconds = 0;
+            minutes++;
+        }
+        else if (minutes === 60 && seconds === 60) {
+            minutes = 0;
+            seconds = 0;
+            hours++;
+        }
+        else if (hours === 24 && minutes === 60 && seconds === 60) {
+            timerClass.innerHTML = `
+        ${timerClass.innerHTML}
+        <div class="error"> <span class="warning-icon">!</span>You have reached your time limit!<div>
+      `;
+            timer = null;
+            clearInterval(timer);
+            return;
+        }
+        timerClass.innerHTML = `${hours <= 9 ? `0${hours}` : hours}:${minutes <= 9 ? `0${minutes}` : minutes}:${seconds <= 9 ? `0${seconds}` : seconds}`;
+        informer = true;
+    }, 1000);
+}
+function stopTimer() {
+    startbtn.innerHTML = 'Resume';
+    clearInterval(timer);
+    informer = false;
+}
+function resetTimer() {
+    hours = 0;
+    minutes = 0;
+    seconds = 0;
+    timerClass.innerHTML = '00:00:00';
+    clearInterval(timer);
+    informer = false;
+}
--- ts/script.ts
+++ ts/script.ts
@@ -1,27 +1,60 @@
-let hours: number = 0, minutes: number = 0, seconds: number = 0;
 const timerClass: HTMLDivElement = document.querySelector('#timer') as HTMLDivElement;
+const startbtn: HTMLButtonElement = document.querySelector('#start') as HTMLButtonElement;
+const stopbtn: HTMLButtonElement = document.querySelector('#stop') as HTMLButtonElement;
+const resetbtn: HTMLButtonElement = document.querySelector('#reset') as HTMLButtonElement;
+let hours: number = 0, minutes: number = 0, seconds: number = 0;
+let timer: any = null;
+let informer: boolean = false;
 
-const timer: number = setInterval(() => {
-  seconds++;
+startbtn.addEventListener('click', () => startTimer());
+stopbtn.addEventListener('click', () => stopTimer());
+resetbtn.addEventListener('click', () => resetTimer());
 
-  if (seconds === 60) {
-    seconds = 0;
-    minutes++;
-  }
-  else if (minutes === 60 && seconds === 60) {
-    minutes = 0;
-    seconds = 0;
-    hours++;
-  }
-  else if (hours === 24 && minutes === 60 && seconds === 60) {
-    timerClass.innerHTML = `
-      ${timerClass.innerHTML}
-      <div class="error"> <span class="warning-icon">!</span>You have reached your time limit!<div>
-    `;
-    clearInterval(timer);
+function startTimer(): void {
+  if (informer) {
     return;
   }
 
+  timer = setInterval(() => {
+    seconds++;
 
-  timerClass.innerHTML = `${hours <= 9 ? `0${hours}` : hours}:${minutes <= 9 ? `0${minutes}` : minutes}:${seconds <= 9 ? `0${seconds}` : seconds}`;
-}, 1000);
+    startbtn.innerHTML === 'Resume' ? startbtn.innerHTML = 'Start' : null;
+
+    if (seconds === 60) {
+      seconds = 0;
+      minutes++;
+    }
+    else if (minutes === 60 && seconds === 60) {
+      minutes = 0;
+      seconds = 0;
+      hours++;
+    }
+    else if (hours === 24 && minutes === 60 && seconds === 60) {
+      timerClass.innerHTML = `
+        ${timerClass.innerHTML}
+        <div class="error"> <span class="warning-icon">!</span>You have reached your time limit!<div>
+      `;
+      timer = null;
+      clearInterval(timer);
+      return;
+    }
+
+
+    timerClass.innerHTML = `${hours <= 9 ? `0${hours}` : hours}:${minutes <= 9 ? `0${minutes}` : minutes}:${seconds <= 9 ? `0${seconds}` : seconds}`;
+    informer = true;
+  }, 1000);
+}
+
+function stopTimer(): void {
+  startbtn.innerHTML = 'Resume';
+  clearInterval(timer);
+  informer = false;
+}
+function resetTimer(): void {
+  hours = 0;
+  minutes = 0;
+  seconds = 0;
+  timerClass.innerHTML = '00:00:00';
+  clearInterval(timer);
+  informer = false;
+}
